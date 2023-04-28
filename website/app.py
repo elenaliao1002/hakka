58f46f66397366a2a53bae816221dbe0accced35
Elena Liao <b03607061@gmail.com>
2023-04-28 13:09:23 -0700

    website bug

--- website/app.py
+++ website/app.py
@@ -18,6 +18,12 @@ def main():
     menu = ["Yolo", "RNN", "CNN"]
     choice = st.sidebar.selectbox("Algorithm", menu)
 
+    folderPath = f'{cwd}/inferenced_imgs'
+    # Check Folder is exists or Not
+    if os.path.exists(folderPath):
+        # Delete Folder code
+        shutil.rmtree(folderPath)
+
     col1, col2 = st.columns(2)
 
     if choice == "Yolo":
@@ -40,11 +46,11 @@ def main():
             col2.image(classified, use_column_width=True,
                        caption="detected Image")
 
-        folderPath = f'{cwd}/inferenced_imgs'
-        # Check Folder is exists or Not
-        if os.path.exists(folderPath):
-            # Delete Folder code
-            shutil.rmtree(folderPath)
+        # folderPath = f'{cwd}/inferenced_imgs'
+        # # Check Folder is exists or Not
+        # if os.path.exists(folderPath):
+        #     # Delete Folder code
+        #     shutil.rmtree(folderPath)
 
 
 def make_inference(SOURCE_PATH):
@@ -55,10 +61,10 @@ def make_inference(SOURCE_PATH):
     my_bar = st.progress(0)
     with st.spinner(progress_text):
         subprocess.run(
-            ['python3', f'{cwd}/models/yolov5/segment/predict.py',
-             '--source', f'{cwd}/website/user_uploads', '--weights',
-             f'{cwd}/models/yolov5/runs/train/exp2/weights/best1.pt',
-             '--project', f'{cwd}', '--name', 'inferenced_imgs',
+            ['python3', f'{pwd}/models/yolov5/segment/predict.py', '--source',
+             f'{pwd}/website/user_uploads/{SOURCE_PATH.name}', '--weights',
+             f'{pwd}/models/yolov5/runs/train/exp2/weights/best.pt',
+             '--project', f'{cwd}', '--name', 'inferenced_imgs', '--save-txt',
              '--hide-conf', '--conf', '0.25'])
         for percent_complete in range(100):
             time.sleep(0.1)
